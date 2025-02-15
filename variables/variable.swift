719d8f4c1972e3701c09f453b0499f287df2f8ac
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-02-15 14:15:59 +0000

    updated

--- variables/variable.swift
+++ variables/variable.swift
@@ -1,9 +1,11 @@
 /* Variables Exercise 
-Warning. Do not change the code on lines 1-4 and 16-20. Your program should work for different inputs. e.g. any value of a and b.
+Write a program that switches the values stored in the variables a and b inside the main.swift file.
+Your program should work for different inputs. e.g. any value of a and b.
 Hint
 - You should not have to type any numbers in your code.
 - You might need to make some more variables. */
 
+// Solution:
 
 var a = 5
 var b = 8
