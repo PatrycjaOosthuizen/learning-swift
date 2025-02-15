46cb551fe43bf1faf3d06721d4f7bf07a5b1e3fa
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-02-15 14:14:23 +0000

    added exercise content

---
+++ variables/variable.swift
@@ -0,0 +1,16 @@
+/* Variables Exercise 
+Warning. Do not change the code on lines 1-4 and 16-20. Your program should work for different inputs. e.g. any value of a and b.
+Hint
+- You should not have to type any numbers in your code.
+- You might need to make some more variables. */
+
+
+var a = 5
+var b = 8
+
+var c = a
+a = b
+b = c
+
+print("a: \(a)")
+print("b: \(b)")
