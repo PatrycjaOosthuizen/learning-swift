8f15bf72b59e7b2429021ab7da694c9922bfdc9f
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-02-15 13:40:09 +0000

    variable swift

---
+++ Variable/Variable Exercise.playground/Contents.swift
@@ -0,0 +1,20 @@
+/*
+Variables Exercise
+Instructions
+Write a program that switches the values stored in the variables a and b inside the main.swift file.
+ Your code should also work for any value. For example if a has the value "Hello" and b is "Jack" then:
+ Hint
+ - You should not have to type any numbers in your code.
+ - You might need to make some more variables.
+ 
+ */
+
+var a = 5
+var b = 8
+
+var c = a
+a = b
+b = c
+
+print("a: \(a)")
+print("b: \(b)")
