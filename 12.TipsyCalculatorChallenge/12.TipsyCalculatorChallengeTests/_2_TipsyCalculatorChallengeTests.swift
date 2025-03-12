4c2743ccaa7fe7e0585373b495e8649b8faf3a21
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-03-12 14:06:33 +0000

    Section 12:TipsyCalculator Challenge initial commit

---
+++ 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallengeTests/_2_TipsyCalculatorChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  _2_TipsyCalculatorChallengeTests.swift
+//  12.TipsyCalculatorChallengeTests
+//
+//  Created by PatrycjaOosthuizen on 12/03/2025.
+//
+
+import Testing
+@testable import _2_TipsyCalculatorChallenge
+
+struct _2_TipsyCalculatorChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
