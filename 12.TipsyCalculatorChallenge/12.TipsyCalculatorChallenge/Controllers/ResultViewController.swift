255b1306efce488466a5ba373c693a5b13a2e9fa
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-03-13 12:42:33 +0000

    Created IBActions and IBOutlets for the Result Screen

--- 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallenge/Controllers/ResultViewController.swift
+++ 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallenge/Controllers/ResultViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var settingslabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +20,6 @@ class ResultViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculatePressed(_ sender: UIButton) {
     }
-    */
-
 }
