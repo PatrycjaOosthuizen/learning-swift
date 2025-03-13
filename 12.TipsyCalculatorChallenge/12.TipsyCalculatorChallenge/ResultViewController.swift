8ea5da9d0a926a7ffe20cc518d22ddea16828774
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-03-13 06:38:34 +0000

    Created Result View Controller Cocoa Touch Class

---
+++ 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallenge/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  12.TipsyCalculatorChallenge
+//
+//  Created by PatrycjaOosthuizen on 13/03/2025.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
