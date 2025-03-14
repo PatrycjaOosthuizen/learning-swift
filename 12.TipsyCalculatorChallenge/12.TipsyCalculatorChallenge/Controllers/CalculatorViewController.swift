6a8d9c5928b6a9c6e1d8279abcce941e63644a5a
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-03-14 05:38:24 +0000

    Apply logic and pass results to ResultViewController

--- 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallenge/Controllers/CalculatorViewController.swift
+++ 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallenge/Controllers/CalculatorViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+var tipPercentage: Float = 0.0
+var splitNumber: Int = 2  // Default to 2 people
+var finalAmount: String = "0.00"  // Store the final calculated amount
+
 class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var billTextField: UITextField!
@@ -24,12 +28,47 @@ class CalculatorViewController: UIViewController {
         
         // Select the button that was pressed
         sender.isSelected = true
+        
+        // Extract tip percentage from button title
+        let buttonTitle = sender.currentTitle ?? "0%"
+        let tipValue = Double(buttonTitle.dropLast()) ?? 0.0
+        tipPercentage = Float(tipValue) / 100.0
+        
+        //  Dismiss the keyboard when a tip button is pressed
+        billTextField.endEditing(true)
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumber = Int(sender.value)  // Store the new split value
+        splitNumberLabel.text = String(splitNumber)  // Update label
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        if let billText = billTextField.text, !billText.isEmpty, let billAmount = Double(billText) {
+            let totalBillWithTip = billAmount * (1 + Double(tipPercentage))
+            let amountPerPerson = totalBillWithTip / Double(splitNumber)
+            
+            // Format the final amount
+            finalAmount = String(format: "%.2f", amountPerPerson)
+
+            // Perform segue
+            performSegue(withIdentifier: "goToResult", sender: self)
+        } else {
+            // Show an alert if the user tries to calculate without entering an amount
+            let alert = UIAlertController(title: "Error", message: "Please enter a bill amount.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+
+    }
+    // The prepare(for segue:) function
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.resultValue = finalAmount
+            destinationVC.tipPercentage = tipPercentage
+            destinationVC.splitNumber = splitNumber
+        }
     }
 }
 
--- 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallenge/Controllers/ResultViewController.swift
+++ 12.TipsyCalculatorChallenge/12.TipsyCalculatorChallenge/Controllers/ResultViewController.swift
@@ -11,15 +11,27 @@ class ResultViewController: UIViewController {
 
     @IBOutlet weak var totalLabel: UILabel!
     
-    @IBOutlet weak var settingslabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    var resultValue: String?  // Store received amount
+    var tipPercentage: Float = 0.0
+    var splitNumber: Int = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //  Display calculated amount per person
+        totalLabel.text = resultValue
+        
+        // Display settings (split & tip percentage)
+        settingsLabel.text = "Split between \(splitNumber) people, with \(Int(tipPercentage * 100))% tip."
     }
     
 
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        // Go back to previous screen
+        dismiss(animated: true, completion: nil)
     }
+    
 }
