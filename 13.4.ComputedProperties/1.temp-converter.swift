701eb8be0d58edcb067b8a3bad2c11c503481228
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-03-25 05:53:44 +0000

    Computed Property Exercise : Temperature Converter

---
+++ 13.4.ComputedProperties/1.temp-converter.swift
@@ -0,0 +1,26 @@
+/*
+Exercise : Temperature Converter
+Create a struct Temperature that stores temperature in Celsius but provides a computed property to get the equivalent value in Fahrenheit.
+
+var aProperty : Type {
+    get {
+       return the value
+    }
+}
+
+*/
+
+struct Temperature {
+    var celsius: Double
+    
+    // Computed property to get temperature in Fahrenheit
+    var fahrenheit: Double {
+        get {
+            return (celsius * 9/5) + 32
+        }
+    }
+}
+
+// Example Usage
+let temp = Temperature(celsius: 25)
+print(temp.fahrenheit) // Output: 77.0
