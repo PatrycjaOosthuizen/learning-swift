98def7c49734b978b8bb16e7e5cb5ff2e83f489a
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-01 14:24:53 +0100

    Worked on app interface and added currency array

---
+++ 14.ByteCoinChallenge/ByteCoinChallenge/CoinManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoinManager.swift
+//  ByteCoinChallenge
+//
+//  Created by PatrycjaOosthuizen on 01/04/2025.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD","BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
