556870c4d87d47d673258705d129bf06314c9441
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-09 05:34:20 +0100

    while loop doubling numbers exercise

---
+++ 15.loops/2.while.loop.swift
@@ -0,0 +1,8 @@
+// Exercise 2:
+// Start with 1, keep doubling the number using a while loop, and stop when it's more than 100.
+
+var number = 1
+while number <= 100 {
+    print(number)
+    number *= 2
+}
