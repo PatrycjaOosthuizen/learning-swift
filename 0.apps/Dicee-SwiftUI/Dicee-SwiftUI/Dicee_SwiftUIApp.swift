355ed9fb002c7d2c2f27482d508256c3f9f74ac0
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-16 07:08:18 +0100

    Dicee-SwiftUI initial commit

---
+++ 0.apps/Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by PatrycjaOosthuizen on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
