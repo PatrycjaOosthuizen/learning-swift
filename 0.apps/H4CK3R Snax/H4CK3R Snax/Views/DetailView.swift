7247757dd75c385cda0f7774ace251cc03b008f6
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-18 07:32:11 +0100

    Created new folder 'Views' and file 'DetailView'

---
+++ 0.apps/H4CK3R Snax/H4CK3R Snax/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  H4CK3R Snax
+//
+//  Created by PatrycjaOosthuizen on 18/04/2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
