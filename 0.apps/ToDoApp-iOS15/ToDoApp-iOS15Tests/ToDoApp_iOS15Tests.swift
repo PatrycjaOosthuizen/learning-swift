26c643755d2424ed2eee99ca0657471325164ec4
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-23 14:43:40 +0100

    ToDoApp Initial commit

---
+++ 0.apps/ToDoApp-iOS15/ToDoApp-iOS15Tests/ToDoApp_iOS15Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_iOS15Tests.swift
+//  ToDoApp-iOS15Tests
+//
+//  Created by PatrycjaOosthuizen on 23/04/2025.
+//
+
+import Testing
+@testable import ToDoApp_iOS15
+
+struct ToDoApp_iOS15Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
