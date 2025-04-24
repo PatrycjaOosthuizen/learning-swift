9b70d30b7cc688e06f5f5d54c9b00d9da9f1e0a9
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-24 07:28:00 +0100

    Add functionality to create and add new to-do items via alert
    This update introduces a user prompt using UIAlertController to allow users to input new to-do items. When the user taps the add button, an alert with a text field appears. The input text is then appended to itemArray, and the table view is refreshed to show the new item.

--- 0.apps/ToDoApp-iOS15/ToDoApp-iOS15/TodoListViewController.swift
+++ 0.apps/ToDoApp-iOS15/ToDoApp-iOS15/TodoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-   var itemArray = ["Buy milk", "Learn Swift", "Go for a walk"]
+    var itemArray = ["Buy milk", "Learn Swift", "Go for a walk"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,4 +46,30 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todo List Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on UIAlert
+            //print(textField.text)
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
