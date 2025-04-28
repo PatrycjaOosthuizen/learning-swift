d3d7104d309b695d15a2ce04b619da88fdade6c4
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-28 14:52:13 +0100

    Create relationships between Category and Item entities in Core Data model

---
+++ 0.apps/ToDoApp-iOS15/ToDoApp-iOS15/Controllers/CategoryViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryViewController.swift
+//  ToDoApp-iOS15
+//
+//  Created by PatrycjaOosthuizen on 28/04/2025.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    //MARK: - TableView Datasource Methods
+    
+    
+    
+    
+    //MARK: - Data Minipulation Methods
+    
+    
+    
+    
+    //MARK: - Add New Categories
+    
+    
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+   
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+    
+}
