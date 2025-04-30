a63495f80f47e3c18cd75f9f7437ba5f9342d26f
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-30 05:56:23 +0100

    InspoQuotes App Initial commit

---
+++ 0.apps/InspoQuotes-iOS15/InspoQuotes-iOS15Tests/InspoQuotes_iOS15Tests.swift
@@ -0,0 +1,17 @@
+//
+//  InspoQuotes_iOS15Tests.swift
+//  InspoQuotes-iOS15Tests
+//
+//  Created by PatrycjaOosthuizen on 30/04/2025.
+//
+
+import Testing
+@testable import InspoQuotes_iOS15
+
+struct InspoQuotes_iOS15Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
