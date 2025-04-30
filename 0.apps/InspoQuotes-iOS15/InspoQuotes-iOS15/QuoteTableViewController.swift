f4ba8b487ec4f09b0056e42bc8f2b0dc531bc9de
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-04-30 06:48:39 +0100

    Implement table view row count for quotes display
    - Added numberOfRowsInSection method to return the count of quotesToShow.
    - Enables dynamic population of the table view based on the quotes array.

--- 0.apps/InspoQuotes-iOS15/InspoQuotes-iOS15/QuoteTableViewController.swift
+++ 0.apps/InspoQuotes-iOS15/InspoQuotes-iOS15/QuoteTableViewController.swift
@@ -29,6 +29,15 @@ class QuoteTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      
+        return quotesToShow.count
     }
 
 
