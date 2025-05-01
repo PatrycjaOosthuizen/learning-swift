d9a64e65b27d52a5ffc97588a7078b71f21a4b0d
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-05-01 13:47:45 +0100

    computed properties exercise (setter)

---
+++ Swift-Playgrounds/ComputedPropertiesSetter.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let pizzaInInches: Int = 12
+
+var numberOfSlices: Int {
+    get {
+        return pizzaInInches - 4
+    }
+    set {
+        print("numberOfSlices now has a new value which is \(newValue)")
+    }
+}
+
+numberOfSlices = 10
+
