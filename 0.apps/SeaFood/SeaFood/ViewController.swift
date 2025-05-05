f61cc49deebe8e2b46d0f8bada53879039fa1189
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-05-05 06:47:34 +0100

    Design and connect IBOutlet and IBAction button

--- 0.apps/SeaFood/SeaFood/ViewController.swift
+++ 0.apps/SeaFood/SeaFood/ViewController.swift
@@ -11,11 +11,15 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
 }
 
