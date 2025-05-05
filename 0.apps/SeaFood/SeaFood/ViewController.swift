3e0220a0c7357615de1021ac50c9b33b73deaeaa
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-05-05 06:35:47 +0100

    CoreML Exercise App initial commit

---
+++ 0.apps/SeaFood/SeaFood/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by PatrycjaOosthuizen on 05/05/2025.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ 0.apps/SeaFood/SeaFoodTests/SeaFoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeaFoodTests.swift
+//  SeaFoodTests
+//
+//  Created by PatrycjaOosthuizen on 05/05/2025.
+//
+
+import Testing
+@testable import SeaFood
+
+struct SeaFoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
