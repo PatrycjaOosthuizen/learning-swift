f4f359a0335b7339039287f254d14e441bff51f5
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-05-08 06:29:36 +0100

    ImageRecognitionApp initial commit
    You’ll build an iOS app that lets a user:
    
    Select a photo from the photo library.
    
    Classify it using my Create ML model (ImageRecognitionMLModel1.mlmodel).
    
    Display the result (label + confidence) on screen.

---
+++ CreateML/ImageRecognitionApp/ImageRecognitionApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ImageRecognitionApp
+//
+//  Created by PatrycjaOosthuizen on 08/05/2025.
+//
+
+import UIKit
+import CoreML
+import Vision
+import PhotosUI
+
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
---
+++ CreateML/ImageRecognitionApp/ImageRecognitionAppTests/ImageRecognitionAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRecognitionAppTests.swift
+//  ImageRecognitionAppTests
+//
+//  Created by PatrycjaOosthuizen on 08/05/2025.
+//
+
+import Testing
+@testable import ImageRecognitionApp
+
+struct ImageRecognitionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
