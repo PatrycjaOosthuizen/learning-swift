9cc57108582c9303040f2165502caef4d4c07dbb
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-05-09 13:48:33 +0100

    Add plane detection visualisation using grid texture on detected AR planes
    Implement renderer callback to visualise detected ARPlaneAnchors with horizontal grid texture overlay.

--- AugmentedRealityApps/ARDicee/ARDicee/ViewController.swift
+++ AugmentedRealityApps/ARDicee/ARDicee/ViewController.swift
@@ -16,6 +16,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Enable visualization of feature points (small yellow dots) that ARKit detects in the real world.
+        // These points help ARKit understand surface geometry for plane detection and tracking.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -23,16 +27,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
        
         
      
-        // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-            
-        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-            
-        sceneView.scene.rootNode.addChildNode(diceNode)
-            
-        }
+//        // Create a new scene
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//        
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//            
+//        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//            
+//        sceneView.scene.rootNode.addChildNode(diceNode)
+//            
+//        }
         
 
 
@@ -44,6 +48,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal // .vertical
         
         // Print whether ARKit is supported on the current device at all.
         // This checks general AR capabilities (usually true for devices with A9 chip or later).
@@ -62,6 +68,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: any SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            // print("plane detected")
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+        } else {
+            return
+        }
+
     }
 
 }
