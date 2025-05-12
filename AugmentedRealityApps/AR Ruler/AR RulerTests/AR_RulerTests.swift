3d586e4ceaf17018690b09c5b9de26557dfe902c
Patrycja Oosthuizen <oosthuizen.code@gmail.com>
2025-05-12 06:38:39 +0100

    AR Ruler initial commit

---
+++ AugmentedRealityApps/AR Ruler/AR RulerTests/AR_RulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_RulerTests.swift
+//  AR RulerTests
+//
+//  Created by PatrycjaOosthuizen on 12/05/2025.
+//
+
+import Testing
+@testable import AR_Ruler
+
+struct AR_RulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
